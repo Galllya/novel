f16b82670b2133c542606bb84c0e62abae3a0328
Galllya <galina.bolganova@gmail.com>
2022-05-01 17:57:29 +0700

    Initial Commit

---
+++ Novel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Novel
+//
+//  Created by Galina Bolganova on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
